baebe491a005f0785bcca95dbeedb398b5e23f12
Ruslan Skalden <russell.skalden@gmail.com>
2019-09-28 09:49:45 -0700

    Send to API

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import KeymasterLan
 import time
 from Server import Server
 import threading
+import requests
+from requests.adapters import HTTPAdapter
 
 KEYMASTER_DRIVER = "LAN" #Can be LAN or SERIAL
+API_URL = 'http://localhost/'
 
 if KEYMASTER_DRIVER == "LAN":
     ser = KeymasterLan.KeymasterLan('192.168.0.178', 5000)
@@ -15,6 +18,25 @@ elif KEYMASTER_DRIVER == "SERIAL":
 def server_handler():
     server = Server('localhost', 80, ser)
     server.start()
+
+
+def send_status(data):
+    try:
+        send_data = ''
+        for i in data:
+            send_data += str(i) + ":" + str(int(data.get(i))) + "::"
+        print(send_data)
+
+        service_api_adapter = HTTPAdapter(max_retries=3)
+        session = requests.Session()
+        session.mount(API_URL, service_api_adapter)
+        response = session.put(API_URL, data={'locker_statuses': send_data}, timeout=1.5)
+        if response.status_code == 200:
+            print('Status sent successfully')
+    except requests.exceptions.Timeout as err:
+        print("API error: request TIMEOUT".format(err))
+    except Exception as err:
+        print("API error: {0}".format(err))
 
 
 def status_handler():
@@ -37,6 +59,7 @@ def status_handler():
 
             print("")
             if changed:
+                send_status(current_state)
                 print("STATE CHANGED SENDING...")
 
         except Exception as err:
